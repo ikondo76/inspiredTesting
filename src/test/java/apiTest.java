f7f32cbd05cad41d9e4da8834acb759121ce59f1
ikondo76 <ignatiuskondo@gmail.com>
2026-05-21 19:50:43 +0200

    created retrieveUser method

---
+++ src/test/java/apiTest.java
@@ -0,0 +1,25 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class apiTest {
+
+    final String BASE_URL = "https://reqres.in/api/users/2";
+    final String API_KEY = "reqres-free-v1";
+
+    public void retrieveUser() {
+        RestAssured.baseURI = BASE_URL;
+
+        Response response = RestAssured
+                .given()
+                .header("reqres-free-v1", API_KEY)
+                .get(BASE_URL);
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        System.out.println("Status Code: " + response.statusCode());
+        System.out.println("First Name: " + response.jsonPath().getString("data.first_name"));
+        System.out.println("Avatar URL: " + response.jsonPath().getString("data.avatar"));
+    }
+}
