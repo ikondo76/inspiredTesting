8863916614a4da1ef1f57f97c2b239a0f687085e
ikondo76 <ignatiuskondo@gmail.com>
2026-05-21 19:58:41 +0200

    created updateEmailAddress method

--- src/test/java/apiTest.java
+++ src/test/java/apiTest.java
@@ -26,5 +26,26 @@ public class apiTest {
         Assert.assertEquals(response.statusCode(), 200, "Expected status code 200");
     }
 
+    public void updateEmailAddress() {
+//        RestAssured.baseURI = BASE_URL;
+        String payload = "{ \"email\": \"ignatius@gmail.com\" }";
+
+        Response res = RestAssured
+                .given()
+                .header("reqres-free-v1", API_KEY)
+                .header("Content-Type", "application/json")
+                .body(payload)
+                .put(BASE_URL)
+                .then()
+                .extract().response();
+
+        Assert.assertEquals(res.statusCode(), 200);
+        Assert.assertEquals(res.jsonPath().getString("email"), "ignatius@example.com");
+    }
+
+
+
 
 }
+
+
